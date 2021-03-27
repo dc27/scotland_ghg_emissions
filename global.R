c0deafa0c5fd2415bafc7be1d6561f67a8d98f3f
david currie <dcurrie27@gmail.com>
2021-03-27 16:14:52 +0000

    working app v1

--- global.R
+++ global.R
@@ -6,7 +6,7 @@ library(leaflet)
 library(rgdal)
 
 la_shapes <- readOGR(
-  dsn = "data/shapefiles/la/scottish_local_authorities/",
+  dsn = "data/shapefiles/la/scottish_local_authorities",
   layer = "scottish_local_authorities",
   GDAL1_integer64_policy = TRUE
 )
--- ui.R
+++ ui.R
@@ -5,11 +5,15 @@ ui <- dashboardPage(
     tags$style(
       type = "text/css",
       "
-      #scotland_map {height:calc(100vh-160px) !important;}
+      #scotland_map {height: calc(100vh - 160px) !important;}
       "
     ),
     box(
+      width = 9,
       leafletOutput("scotland_map")
+    ),
+    column(
+      width = 3
     )
   )
 )

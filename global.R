5c0019e64b251a83e1d0e70998d1c0b9396d6627
david currie <dcurrie27@gmail.com>
2021-03-27 16:39:22 +0000

    adds dataset info to app

--- global.R
+++ global.R
@@ -5,6 +5,18 @@ library(dplyr)
 library(leaflet)
 library(rgdal)
 
+dfs <- list(
+  "Standardised Emissions" = list(
+    data = read_csv("data/clean_data/scot_emissions_summary.csv"),
+    explorable_vars = c("year", "per_captia_emissions_t",
+                        "emissions_per_km2_kt")
+  ),
+  "Raw Emissions" = list(
+    data = read_csv("data/clean_data/scot_emissions.csv"),
+    explorable_vars = c("year", "emissions_source_sector")
+  )
+)
+
 la_shapes <- readOGR(
   dsn = "data/shapefiles/la/scottish_local_authorities",
   layer = "scottish_local_authorities",

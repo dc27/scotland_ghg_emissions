ab055cb6f4c2144f391bc8d04c9228915a425db8
david currie <dcurrie27@gmail.com>
2021-03-31 19:30:39 +0100

    new features - per capita and per sq kilometer standards

--- ui.R
+++ ui.R
@@ -11,37 +11,24 @@ ui <- dashboardPage(
     fluidRow(
       box(
         width = 9,
-        leafletOutput("scotland_map"),
-        absolutePanel(
-          bottom = 0,
-          right = 20,
-          left = 20,
-          tags$style(
-            ".irs-bar {",
-            "  border-color: transparent;",
-            "  background-color: transparent;",
-            "}",
-            ".irs-bar-edge {",
-            "  border-color: transparent;",
-            "  background-color: transparent;",
-            "}"
-          ),
-          sliderInput("year",
-                      "Select year",
-                      value = 2018,
-                      max = max(years), min = min(years),
-                      sep = "",
-                      ticks = TRUE
-          )
-        )
+        leafletOutput("scotland_map")
       ),
       column(
         width = 3,
         pickerInput("subsector",
-                    "Subsector",
+                    "Subsector(s):",
                     choices = subsectors,
                     options = list(`actions-box` = TRUE),multiple = T),
         tags$hr(),
+        sliderInput("year",
+                    "Year",
+                    value = 2018,
+                    max = max(years), min = min(years),
+                    sep = "",
+                    ticks = TRUE
+        ),
+        checkboxInput("per_capita", "per capita", FALSE),
+        checkboxInput("per_sq_km", "per square kilometer", FALSE),
         checkboxInput("legend", "Show legend", TRUE),
         tags$div(style = "text-align:center;",
                  actionButton("update", "Update"))

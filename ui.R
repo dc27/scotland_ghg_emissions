43dea4c613f515741e670232c50069114aab7abe
david currie <dcurrie27@gmail.com>
2021-04-02 14:48:08 +0100

    add empty transport tab

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 ui <- dashboardPage(
-  dashboardHeader(title = "Scotland CO2 Emissions"),
+  dashboardHeader(title = "Scotland Emissions"),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
     tags$style(
@@ -8,30 +8,38 @@ ui <- dashboardPage(
       #scotland_map {height: calc(100vh - 160px) !important;}
       "
     ),
-    fluidRow(
-      box(
-        width = 9,
-        leafletOutput("scotland_map")
+    tabsetPanel(
+      tabPanel(
+        "CO2 Emissions",
+        fluidRow(
+          box(
+            width = 9,
+            leafletOutput("scotland_map")
+          ),
+          column(
+            width = 3,
+            pickerInput("subsector",
+                        "Subsector(s):",
+                        choices = subsectors,
+                        options = list(`actions-box` = TRUE),multiple = T),
+            tags$hr(),
+            sliderInput("year",
+                        "Year",
+                        value = 2018,
+                        max = max(years), min = min(years),
+                        sep = "",
+                        ticks = TRUE
+            ),
+            checkboxInput("per_capita", "per capita", FALSE),
+            checkboxInput("per_sq_km", "per square kilometer", FALSE),
+            checkboxInput("legend", "Show legend", TRUE),
+            tags$div(style = "text-align:center;",
+                     actionButton("update", "Update"))
+          )
+        )
       ),
-      column(
-        width = 3,
-        pickerInput("subsector",
-                    "Subsector(s):",
-                    choices = subsectors,
-                    options = list(`actions-box` = TRUE),multiple = T),
-        tags$hr(),
-        sliderInput("year",
-                    "Year",
-                    value = 2018,
-                    max = max(years), min = min(years),
-                    sep = "",
-                    ticks = TRUE
-        ),
-        checkboxInput("per_capita", "per capita", FALSE),
-        checkboxInput("per_sq_km", "per square kilometer", FALSE),
-        checkboxInput("legend", "Show legend", TRUE),
-        tags$div(style = "text-align:center;",
-                 actionButton("update", "Update"))
+      tabPanel(
+        "Transport"
       )
     )
   )

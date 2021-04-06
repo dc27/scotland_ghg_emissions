3a09428e4b7e66c62889fb58112a97e59576be11
david currie <dcurrie27@gmail.com>
2021-04-06 22:25:15 +0100

    tests for dynamic output

--- ui.R
+++ ui.R
@@ -2,7 +2,8 @@ ui <- dashboardPage(
   dashboardHeader(title = "Scotland Emissions"),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
-    selectInput("col_choice", "Category Method", colnames(emissions_data)),
-    uiOutput("dropdowns")
+    selectInput("col_choice", "Category Method", colnames(emissions_data)[1:3]),
+    uiOutput("dropdowns"),
+    textOutput("test_text")
   )
 )

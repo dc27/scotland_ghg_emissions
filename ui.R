b88e6486a26003a9b5c9fe18dbf8faf7f70d1b00
david currie <dcurrie27@gmail.com>
2021-05-06 14:58:18 +0100

    begins ui changes - new design

--- ui.R
+++ ui.R
@@ -1,110 +1,55 @@
 ui <- dashboardPage(
-  dashboardHeader(title = "Scotland Emissions"),
+  dashboardHeader(title = "Scotland Emissions - Path to Net Zero",
+                  titleWidth = 500),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
+    # specify box heights in css
     tags$style(
       type = "text/css",
       "
-      #totals_plot {height: calc(100vh - 200px) !important;}
-      #emissions_breakdowns {height: calc(100vh - 200px) !important;}
-      #transport_main {height: calc(100vh - 50vh) !important;}
-      #new_ulevs {height: calc(100vh - 50vh) !important;}
-      #road_traffic {height: calc(100vh - 50vh) !important;}
+      #data_select {height: calc(100vh - 35vh) !important;}
+      #plot_box {height: calc(100vh - 35vh) !important;}
+      div.box.box-solid.box-primary {text-align: right;}
+      #extra_info {height: 15vh !important;}
       "
     ),
-    tabsetPanel(
-      tabPanel(
-        "Overview",
-        fluidRow(
-          column(
-            3,
-            selectInput("year_select", "Year: ", 
-                        choices = years,
-                        selected = "2018"),
-            pickerInput("gas_select", "Pollutant(s): ", 
-                        choices = pollutants,
-                        selected = "CO2",
-                        options = list(`actions-box` = TRUE),
-                        multiple = TRUE),
-            radioButtons("facet_select", "Faceting: ",
-                        choices = c("single", "multiple"),
-                        selected = "single"),
-            tags$br(),
-            actionButton("update", "Update")
+    fluidPage(
+      fluidRow(
+        # main ui select
+        box(
+          id = "data_select",
+          title = "Sector",
+          solidHeader = TRUE,
+          status = "success",
+          width = 4,
+          selectInput(
+          "user_sector",
+           label = "Select Sector:",
+           choices = c("All", "Transport"),
+           selected = "All"
           ),
-          column(
-            9,
-            tabBox(
-              width = 12,
-              tabPanel(
-                "Totals",
-                plotlyOutput("totals_plot")
-              ),
-              tabPanel(
-                "Breakdown",
-                plotlyOutput("emissions_breakdowns")
-              ),
-              tabPanel(
-                "Temporal"
-              )
-            )
-          )
+          selectInput(
+            "dataset", label = "Select Dataset:", choices = NULL
+          ),
+          tags$hr(),
+          uiOutput("dynamic_dropdowns")
+        ),
+        # chart
+        box(
+          id = "plot_box",
+          solidHeader = TRUE,
+          status = "primary",
+          title = paste0("Plot : ", "Selected"),
+          width = 8,
         )
       ),
-      tabPanel(
-        "Transport",
-        fluidRow(
-          column(
-            3,
-          selectInput("year_transport", "Year: ",
-                      choices = years,
-                      selected = "2018")
-          ),
-          column(
-            3,
-            tags$br(),
-            actionButton("update_transport", "Update")
-          ),
-          column(
-            3,
-            selectInput("body_type", "Body Type: ",
-                        choices = body_types,
-                        selected = "Cars")
-          ),
-          column(
-            3,
-            selectInput("vehicle_type", "Vehicle Type: ",
-                        choices = vehicle_types,
-                        selected = "Car")
-          )
-        ),
-        fluidRow(
-          column(
-            6,
-            box(
-              width = 12,
-              "Summary",
-              plotlyOutput("transport_main")
-            )
-          ),
-          column(
-            6,
-            tabBox(
-              width = 12,
-              tabPanel(
-                "Newly Registered ULEVs",
-                plotOutput("new_ulevs")
-              ),
-              tabPanel(
-                "Licensed Vehicles",
-                plotOutput("licensend_vehicles")
-                ),
-              tabPanel(
-                "Road Transport",
-                plotOutput("road_traffic")
-              )
-            )
-          )
+      fluidRow(
+        box(
+          id = "extra_info",
+          title = "Information and Links",
+          solidHeader = TRUE,
+          status = "success",
+          width = 12
         )
       )
     )

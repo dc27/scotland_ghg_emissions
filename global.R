18b0eea7cd8a21d6bc4375db15101873496ef5af
david currie <dcurrie27@gmail.com>
2021-05-14 14:27:57 +0100

    removes unused code from global app file

--- global.R
+++ global.R
@@ -21,31 +21,5 @@ dropdown_lookup <- dropdown_lookup
 theme_set(theme_bw())
 
 
-emissions_data <- read_csv("data/clean_data/ghg_emissions.csv")
-
-years <- sort(unique(emissions_data$year))
-pollutants <- unique(emissions_data$pollutant)
-sectors <- sort(unique(emissions_data$ccp_mapping))
-
-
-hierarchical_emissions <- read_csv("data/clean_data/hierarchical_data.csv")
-
-# filtered for only relating to transport
-transport_hierarchy <- hierarchical_emissions %>% 
-  filter(str_detect(id, "^Transport")) %>% 
-  filter(!value < 0)
-
-road_traffic <- read_csv("data/clean_data/transport/road_traffic.csv")
-
-vehicle_types <- unique(road_traffic$vehicle_type)
-
-new_ulevs <- read_csv(
-  "data/clean_data/transport/newly_registered_vehicles_and_ulevs.csv"
-  )
-
-body_types <- unique(new_ulevs$body_type)
-
-
-
 
 

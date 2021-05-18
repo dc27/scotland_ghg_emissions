726b81875c6526ac0ee88f2535b7aedbb469563a
david currie <dcurrie27@gmail.com>
2021-05-18 17:52:40 +0100

    adds footer

--- ui.R
+++ ui.R
@@ -1,304 +1,314 @@
-ui <- dashboardPage(
-  dashboardHeader(title = "Scotland Emissions - Path to Net Zero",
-                  titleWidth = 500),
-  dashboardSidebar(disable = TRUE),
-  dashboardBody(
-    # call shinyjs
-    useShinyjs(),
-    # specify box heights in css
-    tagList(
-      # ----- css -----
-      # TODO:: write separate css file
-      tags$head(
-        tags$style(
-          type = 'text/css',
-          '
-          header.main-header{
-          display:none;
-          }
-          .navbar{
-          display:none;
-          }
-          .no_padding_col {
-          padding: 0;
-          }
-          .bttn-simple {
-          width:100%;
-          height:10vh;
-          border-radius: 0;
-          }
-          #goto_emissions_explorer {
-          margin-bottom: 1vh;
-          height:20vh;
-          width:100%;
-          background-color: white;
-          color:black;
-          font-size: 20px;
-          font-family: inherit;
-          padding: 5px 12px;
-          }
-          .bttn-bordered {
-          background-color: white;
-          width:100%;
-          height:10vh;
-          }
-          .box-solid.bg-blue {
-          background-color: lightblue;
-          margin:1vh;
-          }
-          h2{
-          text-align: center;
-          }
-          .verti-hori-center {
-            margin: 0;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            -ms-transform: translate(-50%, -50%);
-            transform: translate(-50%, -50%);
-          }
-          .box-header {
-            height:0;
-            padding:0;
-          }
-          .info-box{
-          height:14vh;
-          margin:1vh;
-          }
-          .info-box-icon{
-          height:14vh;
-          width:14vh;
-          }
-          .info-box-content{
-          text-align:right;
-          }
-          #emissions_plot
-          {height: calc(100vh - 23vh) !important;}
-          #sinks_plot
-          {height: calc(100vh - 23vh) !important;}
-          '
-        )
-      )
+ui <- tagList(
+  dashboardPage(
+    dashboardHeader(
+      title = "Scotland Emissions - Path to Net Zero",
+      titleWidth = 500
     ),
-    navbarPage(
-      'Test App', collapsible = TRUE, id = "inTabset",
-      # ----- Home Page -----
-      tabPanel(
-        title = "Home", value = "home",
-        fluidRow(
-          column(
-            12,
-            box(
-              title = "",
-              width = "100%",
-              height = "20vh",
-              background = "light-blue",
-              tags$div(
-                class = "verti-hori-center",
-                tags$h1(
-                  "Greenhouse Gas Emissions in Scotland - Path to Net Zero"
+    dashboardSidebar(disable = TRUE),
+    dashboardBody(
+      # call shinyjs
+      useShinyjs(),
+      # specify box heights in css
+      tagList(
+        # ----- css -----
+        # TODO:: write separate css file
+        tags$head(
+          tags$style(
+            type = 'text/css',
+            '
+            header.main-header{
+            display:none;
+            }
+            .navbar{
+            display:none;
+            }
+            .no_padding_col {
+            padding: 0;
+            }
+            .bttn-simple {
+            width:100%;
+            height:10vh;
+            border-radius: 0;
+            }
+            #goto_emissions_explorer {
+            margin-bottom: 1vh;
+            height:20vh;
+            width:100%;
+            background-color: white;
+            color:black;
+            font-size: 20px;
+            font-family: inherit;
+            padding: 5px 12px;
+            }
+            .bttn-bordered {
+            background-color: white;
+            width:100%;
+            height:10vh;
+            }
+            .box-solid.bg-blue {
+            background-color: lightblue;
+            margin:1vh;
+            }
+            h2{
+            text-align: center;
+            }
+            .verti-hori-center {
+              margin: 0;
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              -ms-transform: translate(-50%, -50%);
+              transform: translate(-50%, -50%);
+            }
+            .box-header {
+              height:0;
+              padding:0;
+            }
+            .info-box{
+            height:14vh;
+            margin:1vh;
+            }
+            .info-box-icon{
+            height:14vh;
+            width:14vh;
+            }
+            .info-box-content{
+            text-align:right;
+            }
+            #emissions_plot
+            {height: calc(100vh - 23vh) !important;}
+            #sinks_plot
+            {height: calc(100vh - 23vh) !important;}
+            footer {
+            background-color: grey
+            margin-bottom:0;
+            }
+            '
+          )
+        )
+      ),
+      navbarPage(
+        'Test App', collapsible = TRUE, id = "inTabset",
+        # ----- Home Page -----
+        tabPanel(
+          title = "Home", value = "home",
+          fluidRow(
+            column(
+              12,
+              box(
+                title = "",
+                width = 12,
+                height = "20vh",
+                background = "light-blue",
+                tags$div(
+                  class = "verti-hori-center",
+                  tags$h1(
+                    "Greenhouse Gas Emissions in Scotland - Path to Net Zero"
+                  )
                 )
               )
             )
-          )
-        ),
-        fluidRow(
-          column(
-            4,
-            id = "nav",
-            tags$button(
-              id = "goto_emissions_explorer",
-              class = "btn action-button",
-              tags$img(src = myImgResources, height = "100%"),"Emissions Explorer"
-              
-            ),
-            # actionBttn(
-            #   style = "simple", color = "success",
-            #   inputId = 'goto_emissions_explorer',
-            #   label = 'Emissions Explorer',
-            #   icon = icon("images/sunburst_emissions.png")
-            # ),
-           tags$br(),
-           column(
-             6,
-             class = "no_padding_col",
-             actionBttn(
-               style = "simple", color = "success",
-               icon = icon("tractor"),
-               inputId = 'goto_agriculture',
-               label = 'Agriculture'
-             ),
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_transport',
-               icon = icon("car"),
-               label = 'Transport'
-             ),
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_industry',
-               icon = icon("industry"),
-               label = 'Industry'
-             ),
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_landuse',
-               icon = icon("sync"),
-               label = 'Land Use'
-             )
-           ),
-           column(
-             6,
-             class = "no_padding_col",
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_electricity',
-               icon = icon("bolt"),
-               label = 'Electricity Generation'
-             ),
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_services',
-               icon = icon("asterisk"),
-               label = 'Services'
-             ),
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_waste',
-               icon = icon("trash"),
-               label = 'Waste'
-             ),
-             actionBttn(
-               style = "simple", color = "success",
-               inputId = 'goto_residential',
-               icon = icon("building"),
-               label = 'Residential'
-             )
-           )
-           # actionBttn(
-           #   style = "simple", color = "success",
-           #   inputId = 'goto_targets',
-           #   label = 'Targets Tracker'
-           #  ),
-          )
-        )
-      ),
-      # ------ Emissions Exploration Page -----
-      tabPanel(
-        title = "Emissions Explorer", value = "emissions", 
-        fluidRow(
-          column(
-            2,
-            actionBttn(
-              style = "bordered", color = "success", icon = icon("home"),
-              inputId = 'return_home_1',
-              label = 'Home'
-            )
           ),
-          column(
-            10,
-            tags$div(
-              class = "header_box",
-              box(
-                title = "",
-                width = "100%",
-                height = "10vh",
-                background = "light-blue",
-                tags$h2("Path to Net Zero - Emissions Hub")
-              )
-            )
-          )
-        ),
-        navlistPanel(
-          widths = c(2,10),
-          "Emissions Hub",
-          tabPanel(
-            "Sector Breakdowns",
-            tabBox(
-              width = 12,
-              tabPanel(
-                "Emissions",
-                plotlyOutput("emissions_plot")
+          fluidRow(
+            column(
+              4,
+              id = "nav",
+              tags$button(
+                id = "goto_emissions_explorer",
+                class = "btn action-button",
+                tags$img(src = myImgResources, height = "100%"),"Emissions Explorer"
+                
               ),
-              tabPanel(
-                "Sinks",
-                plotlyOutput("sinks_plot")
+             tags$br(),
+             column(
+               6,
+               class = "no_padding_col",
+               actionBttn(
+                 style = "simple", color = "success",
+                 icon = icon("tractor"),
+                 inputId = 'goto_agriculture',
+                 label = 'Agriculture'
+               ),
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_transport',
+                 icon = icon("car"),
+                 label = 'Transport'
+               ),
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_industry',
+                 icon = icon("industry"),
+                 label = 'Industry'
+               ),
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_landuse',
+                 icon = icon("sync"),
+                 label = 'Land Use'
+               )
+             ),
+             column(
+               6,
+               class = "no_padding_col",
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_electricity',
+                 icon = icon("bolt"),
+                 label = 'Electricity Generation'
+               ),
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_services',
+                 icon = icon("asterisk"),
+                 label = 'Services'
+               ),
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_waste',
+                 icon = icon("trash"),
+                 label = 'Waste'
+               ),
+               actionBttn(
+                 style = "simple", color = "success",
+                 inputId = 'goto_residential',
+                 icon = icon("building"),
+                 label = 'Residential'
+               )
+             )
+             # actionBttn(
+             #   style = "simple", color = "success",
+             #   inputId = 'goto_targets',
+             #   label = 'Targets Tracker'
+             #  ),
+            )
+          )
+        ),
+        # ------ Emissions Exploration Page -----
+        tabPanel(
+          title = "Emissions Explorer", value = "emissions", 
+          fluidRow(
+            column(
+              2,
+              actionBttn(
+                style = "bordered", color = "success", icon = icon("home"),
+                inputId = 'return_home_1',
+                label = 'Home'
+              )
+            ),
+            column(
+              10,
+              tags$div(
+                class = "header_box",
+                box(
+                  title = "",
+                  width = 12,
+                  height = "10vh",
+                  background = "light-blue",
+                  tags$h2("Path to Net Zero - Emissions Hub")
+                )
               )
             )
           ),
-          tabPanel(
-            "Historical Emissions"
+          navlistPanel(
+            widths = c(2,10),
+            "Emissions Hub",
+            tabPanel(
+              "Sector Breakdowns",
+              tabBox(
+                width = 12,
+                tabPanel(
+                  "Emissions",
+                  plotlyOutput("emissions_plot")
+                ),
+                tabPanel(
+                  "Sinks",
+                  plotlyOutput("sinks_plot")
+                )
+              )
+            ),
+            tabPanel(
+              "Historical Emissions"
+            )
           )
         ),
-        tags$footer()
-      ),
-      # ----- Scottish Government Targets Exploration Page -----
-      tabPanel(
-        title = "Scottish Government Targets and Progress",
-        value = "targets", 
-        actionButton('return_home_2', 'Home')
-      ),
-      # ------ Impact of green choices page ----- 
-      tabPanel(
-        title = "Impact of Green Choices",
-        value = "choices",
-        actionButton('return_home_3', 'Home'),
-        fluidRow(
-          # main ui select
-          box(
-            id = "data_select",
-            title = "Sector",
-            solidHeader = TRUE,
-            status = "success",
-            width = 4,
-            height = "70vh",
-            selectInput(
-              "user_sector",
-              label = "Select Sector:",
-              choices = names(dfs),
-              selected = "All"
+        # ----- Scottish Government Targets Exploration Page -----
+        tabPanel(
+          title = "Scottish Government Targets and Progress",
+          value = "targets", 
+          actionButton('return_home_2', 'Home')
+        ),
+        # ------ Impact of green choices page ----- 
+        tabPanel(
+          title = "Impact of Green Choices",
+          value = "choices",
+          actionButton('return_home_3', 'Home'),
+          fluidRow(
+            # main ui select
+            box(
+              id = "data_select",
+              title = "Sector",
+              solidHeader = TRUE,
+              status = "success",
+              width = 4,
+              height = "70vh",
+              selectInput(
+                "user_sector",
+                label = "Select Sector:",
+                choices = names(dfs),
+                selected = "All"
+              ),
+              selectInput(
+                "user_dataset", label = "Select Dataset:", choices = NULL
+              ),
+              tags$hr(),
+              uiOutput("dynamic_dropdowns"),
+              tags$hr(),
+              # updates based on user input
+              radioButtons(
+                "user_plot", label = "Plot Type:", choices = ""
+              ),
+              checkboxInput("p_new_ulevs", "View Percentage of New Regs are ULEV"),
+              tags$div(
+                style = "text-align:center;",
+                actionButton("update", "Confirm Selection")
+              )
             ),
-            selectInput(
-              "user_dataset", label = "Select Dataset:", choices = NULL
-            ),
-            tags$hr(),
-            uiOutput("dynamic_dropdowns"),
-            tags$hr(),
-            # updates based on user input
-            radioButtons(
-              "user_plot", label = "Plot Type:", choices = ""
-            ),
-            checkboxInput("p_new_ulevs", "View Percentage of New Regs are ULEV"),
-            tags$div(
-              style = "text-align:center;",
-              actionButton("update", "Confirm Selection")
+            # chart
+            box(
+              id = "vis_box",
+              width = 8,
+              title = textOutput("title"),
+              height = "70vh",
+              tabPanel(
+                "Chart",
+                plotlyOutput("plot", height = "62vh")
+              )
             )
           ),
-          # chart
-          box(
-            id = "vis_box",
-            width = 8,
-            title = textOutput("title"),
-            height = "70vh",
-            tabPanel(
-              "Chart",
-              plotlyOutput("plot", height = "62vh")
+          fluidRow(
+            # TODO::
+            # additional information, links
+            box(
+              id = "extra_info",
+              title = "Information and Links",
+              solidHeader = TRUE,
+              status = "success",
+              width = 12,
+              height = "20vh"
             )
-          )
-        ),
-        fluidRow(
-          # TODO::
-          # additional information, links
-          box(
-            id = "extra_info",
-            title = "Information and Links",
-            solidHeader = TRUE,
-            status = "success",
-            width = 12,
-            height = "20vh"
           )
         )
       )
     )
-  )
+  ),
+  tags$footer("My footer", align = "center", style = "
+                position:absolute;
+                bottom:0;
+                width:100%;
+                height:50px;   /* Height of the footer */
+                color: white;
+                padding: 10px;
+                background-color: black;
+                z-index: 1000;")
 )
